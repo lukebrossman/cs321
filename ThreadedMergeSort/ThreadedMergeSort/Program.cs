c10afdfbdbedd89dcf884ae85c00f9a0e50fcca2
lukebrossman <lukebrossman1@gmail.com>
2018-12-03 22:57:55 -0800

    Threaded sort HW10

---
+++ ThreadedMergeSort/ThreadedMergeSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadedMergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list of lists so we can easily sort multiple lengths in a loop
+            List<List<int>> randomNumberLists = new List<List<int>> { };
+            //various lengths of the lists to sort
+            int[] lengths = { 8, 64, 256, 1024 };
+            int i = 0;
+            foreach (var length in lengths)
+            {
+                randomNumberLists.Add(new List<int> { });
+                foreach (var num in Enumerable.Range(0, length))
+                {
+                    var rand = new Random();
+                    randomNumberLists[i].Add(rand.Next(length + 100));
+                }
+                i++;
+            }
+
+            foreach (var list in randomNumberLists)
+            {
+                Console.Write("===========Length: " + list.Count + "=======================\n");
+                Console.Write("///Unthreaded\\\\\n");
+                long start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                Console.Write("     Start: " + start + "\n");
+                Sorter.MergeSort(list);
+                long end = DateTimeOffset.Now.ToUnixTimeMilliseconds();                Console.Write("     End: " + end + "\n");
+                Console.Write("           Time in seconds: " + (end - start) / 1000 + "\n\n");
+
+                Console.Write("///Threaded\\\\\n");
+                start = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                Console.Write("     Start: " + start + "\n");
+                Sorter.ThreadedMergeSort(list);
+                end = DateTimeOffset.Now.ToUnixTimeMilliseconds();                Console.Write("     End: " + end + "\n");
+                Console.Write("           Time in seconds: " + (end - start) / 1000 + "\n\n");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ ThreadedMergeSort/ThreadedMergeSort/Sorter.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadedMergeSort
+{
+    /// <summary>
+    /// static class for to contain sorting algorithms
+    /// </summary>
+    static class Sorter
+    {
+        /// <summary>
+        /// Performs a standard mergesort
+        /// </summary>
+        /// <param name="numbers">list of integers to sort</param>
+        /// <returns>sorted list of integers</returns>
+        public static List<int> MergeSort(List<int> numbers)
+        {
+            List<int> left, right;
+            List<int> sorted = new List<int> { };
+
+            if (numbers.Count <= 1)
+            {
+                sorted = numbers;
+            }
+            else
+            {
+                int midPoint = numbers.Count / 2;
+                left = numbers.GetRange(0, midPoint);
+                right = numbers.GetRange(midPoint, numbers.Count - midPoint);
+
+                left = MergeSort(left);
+                right = MergeSort(right);
+                sorted = merge(left, right);
+            }
+
+            return sorted;
+        }
+
+        /// <summary>
+        /// combines to lists of integers into one sorted list of integers
+        /// </summary>
+        /// <param name="left">left list of ints</param>
+        /// <param name="right"> right list of ints</param>
+        /// <returns>sorted combo of both lists</returns>
+        private static List<int> merge(List<int> left, List<int> right)
+        {
+            List<int> merged = new List<int> { };
+            int leftIndex = 0, rightIndex = 0, mergeIndex = 0;
+ 
+            while (leftIndex < left.Count || rightIndex < right.Count)
+            { 
+                if (leftIndex < left.Count && rightIndex < right.Count)
+                {
+                    if (left[leftIndex] <= right[rightIndex])
+                    {
+                        merged.Add(left[leftIndex]);
+                        leftIndex++;
+                        mergeIndex++;
+                    }
+                    else
+                    {
+                        merged.Add(right[rightIndex]);
+                        rightIndex++;
+                        mergeIndex++;
+                    }
+                }
+                else if (leftIndex < left.Count)
+                {
+                    merged.Add(left[leftIndex]);
+                    leftIndex++;
+                    mergeIndex++;
+                }
+                else if (rightIndex < right.Count)
+                {
+                    merged.Add(right[rightIndex]);
+                    rightIndex++;
+                    mergeIndex++;
+                }
+
+            }
+            return merged;
+        }
+        /// <summary>
+        /// Performs a mergesort by creating a thread for every recursive call
+        /// </summary>
+        /// <param name="numbers">list of integers to sort</param>
+        /// <returns>sorted list of integers</returns>
+        public static List<int> ThreadedMergeSort(List<int> numbers)
+        {
+            List<int> left, right;
+            List<int> sorted = new List<int> { };
+
+            if (numbers.Count <= 1)
+            {
+                sorted = numbers;
+            }
+            else
+            {
+                int midPoint = numbers.Count / 2;
+                left = numbers.GetRange(0, midPoint);
+                right = numbers.GetRange(midPoint, numbers.Count - midPoint);
+                var leftThread = new Thread(new ThreadStart(delegate ()
+                {
+                    ThreadedMergeSort(left);
+                }));
+                var rightThread = new Thread(new ThreadStart(delegate ()
+                {
+                    ThreadedMergeSort(right);
+                }));
+                leftThread.Start();
+                rightThread.Start();
+                leftThread.Join();
+                rightThread.Join();
+                sorted = merge(left, right);
+            }
+            return sorted;
+        }
+    }
+}
